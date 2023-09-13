b68846ec327a7b5f60bdbaa7a99c785951e6adfb
Adarsh Rathore <rajputadarsh2@gmail.com>
2023-09-13 20:34:20 +0530

    prject polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        product p = new product();
 
+        int ans1 = p.product(2,3);
+        int ans2 = p.product(1,2,3);
+        double ans3 = p.product(1.0,2.0);
+
+        System.out.println(ans1+" "+ans2+" "+ans3);
+    }
+    public static  class product{
+        public int product(int x,int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x,double y){
+            return x*y;
+        }
+    }
 }
