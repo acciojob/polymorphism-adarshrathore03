580a5c5b03be4c1885bc7b6de55c587e022d58b9
Adarsh Rathore <rajputadarsh2@gmail.com>
2023-09-13 20:52:21 +0530

    project poly

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,20 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Product p = new Product();
 
-        int ans1 = p.Product(2,3);
-        int ans2 = p.Product(1,2,3);
-        double ans3 = p.Product(1.0,2.0);
+        int ans1 = p.product(2,3);
+        int ans2 = p.product(1,2,3);
+        double ans3 = p.product(1.0,2.0);
 
         System.out.println(ans1+" "+ans2+" "+ans3);
     }
     public static  class Product{
-        public int Product(int x,int y){
+        public int product(int x,int y){
             return x*y;
         }
-        public int Product(int x,int y,int z){
+        public int product(int x,int y,int z){
             return x*y*z;
         }
-        public double Product(double x,double y){
+        public double product(double x,double y){
             return x*y;
         }
     }
